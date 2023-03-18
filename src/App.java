4cafb994cb98f0fedf30aa790cf1224bf92ebac2
Victor Silva <victor.silvaPC@hotmail.com>
2023-03-18 15:03:27 -0300

    :tada: initial commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import structure.MStack;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        System.out.println("Learning Data Structure & Algorithms with Java");
+        
+        // Learning Stack
+        MStack.run();
+
+    }
+}
---
+++ src/structure/MStack.java
@@ -0,0 +1,57 @@
+package structure;
+
+import java.util.Stack;
+
+public class MStack {
+
+	public static void run() {
+		
+		/**
+		 * Stack = LIFO data Structure. Last-In First-Out
+		 * 			stores objects into a sort of "vertical tower"
+		 * 			push() to add to the top
+		 * 			pop() to remove from the top
+		 */
+		
+		Stack<String> stack = new Stack<String>();
+
+		// Adding
+		stack.push("Minecraft");
+		stack.push("Skyrim");
+		stack.push("DOOM");
+		stack.push("Borderlands");
+		stack.push("FFVII");
+		
+		// Removing the last item
+
+		String ffvvii = stack.pop(); // remove FFVII
+		System.out.println("popped game: " + ffvvii);
+
+		// getting the last item
+		String borderlands = stack.peek();
+		System.out.println(borderlands);
+		
+		
+		// Searching | The item start at the index 1
+		// If the item doesn't exist, the method will return -1
+		String searchTerm = "Minecraft";
+		System.out.println("Index of " + searchTerm + ": " + stack.search(searchTerm));
+		
+		
+		// Printing the Entire stack
+		System.out.println(stack);
+
+		/**
+		 * 
+		 * Usages
+		 * 
+		 * 1. undo/redo features in text editors
+		 * 2. moving back/forward through browser history
+		 * 3. backtracking algorithms (maze, file directories)
+		 * 4. calling functions (call stack)
+		 * 
+		 */
+		
+	}
+
+}
